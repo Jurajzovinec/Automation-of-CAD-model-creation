1ae235e775839bdc28396ba677693979c0553e21
Juraj Zovinec <juraj.zovinecc@gmail.com>
2020-07-26 20:00:45 +0200

    Creoson folder deletion

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This application is dedicated to creation of CAD master model with use of 
 creoson application and creopyson API. 
+ 
--- creoson/setvars.bat
+++
@@ -1,5 +0,0 @@
-set PROE_COMMON=C:\Program Files\PTC\Creo 4.0\M080\Common Files
-set PROE_ENV=x86e_win64
-
-set JAVA_HOME=jre
-set JSON_PORT=9056
